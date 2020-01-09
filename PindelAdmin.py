947eaf0a26926f6c63b9fcad54e816d890fa201b
AkashCiel <36273163+AkashCiel@users.noreply.github.com>
2020-01-09 23:54:16 +0100

    Add files via upload

--- PindelAdmin.py
+++ PindelAdmin.py
@@ -12,20 +12,21 @@ import numpy as np
 from PindelFunctions import *
 
 # Specify total number of sequences
-totalReadPairs = 285
+totalReadPairs = 1635
 
 # Read DNA sequences from text
 # Import text files in Python
-referenceGenome: TextIO = open("PindelReferenceGenome.txt")
-mainFile: TextIO = open("SamFileSmall.txt")
+referenceGenome: TextIO = open("PindelReferenceLarge.txt")
+mainFile: TextIO = open("SamFileLarge.txt")
 
 # Read reference genome
 refGenomeSeq = referenceGenome.readlines()
-refGenome = np.array([])
+refGenomeSTR = ''
 for i in range(0, len(refGenomeSeq)):
     refGenomeSeq[i] = refGenomeSeq[i].rstrip('\n')
-    refGenome = np.append(refGenome, refGenomeSeq[i])
+    refGenomeSTR += refGenomeSeq[i]
 
+refGenome = np.array([refGenomeSTR])
 # Read SAM
 samReads = mainFile.readlines()
 for i in range(0, len(samReads)):
@@ -40,10 +41,11 @@ for i in range(totalReadPairs):
     readPairs[i].append(samReads[2*i + 1][9])
 
 allMinMaxSubstrings = []
-readLength = 50
-insertSize = 100
-maxDeletionSize = 200
+readLength = 100
+insertSize = 200
+maxDeletionSize = 400
 
+# Looking for deletions in the reference genome
 for i in range(totalReadPairs):
     allMinMaxSubstrings.append([])
     # If left read is mapped and right is not, start looking from 3' end of left read
@@ -57,22 +59,25 @@ for i in range(totalReadPairs):
             allMinMaxSubstrings[i].append(1)
             allMinMaxSubstrings[i].append(minMax3prime)
             allMinMaxSubstrings[i].append(minMax5prime)
+            #print(np.char.find(refGenome[0], minMax3prime[0]) + len(minMax3prime[1]), readPairs[i][1], allMinMaxSubstrings[i])
         else:
             allMinMaxSubstrings[i].append(1)
             allMinMaxSubstrings[i].append([])
             allMinMaxSubstrings[i].append([])
 
-    # If right read is mapped and left if not, look upto the 3' end of right read
+    # If right read is mapped and left is not, look upto the 3' end of right read
     elif np.char.find(refGenome[0], readPairs[i][0]) == -1 and np.char.find(refGenome[0], readPairs[i][1]) > -1:
         anchorPoint = np.char.find(refGenome[0], readPairs[i][1])
-        minMax3prime = minmaxSubstrings(refGenome[0][max(0, anchorPoint - 2*insertSize):anchorPoint], readPairs[i][0], reverse=True)
-        if minMax3prime != []:
-            threePrimeAnchor = np.char.find(refGenome[0][max(0, anchorPoint - 2*insertSize):anchorPoint][::-1], minMax3prime[0][::-1])
-            minMax5prime = minmaxSubstrings(refGenome[0][max(0, anchorPoint - threePrimeAnchor - readLength - maxDeletionSize)
-                                                         :anchorPoint - threePrimeAnchor], readPairs[i][0], reverse=True)
+        minMax5prime = minmaxSubstrings(refGenome[0][max(0, anchorPoint - 2*insertSize):anchorPoint], readPairs[i][0], reverse=True)
+        if minMax5prime != []:
+            fivePrimeAnchor = np.char.find(refGenome[0][max(0, anchorPoint - 2*insertSize):anchorPoint][::-1], minMax5prime[0][::-1])
+            minMax3prime = minmaxSubstrings(refGenome[0][max(0, anchorPoint - fivePrimeAnchor - readLength - maxDeletionSize)
+                                                         :anchorPoint - fivePrimeAnchor], readPairs[i][0], reverse=False)
             allMinMaxSubstrings[i].append(0)
             allMinMaxSubstrings[i].append(minMax3prime)
             allMinMaxSubstrings[i].append(minMax5prime)
+            #if minMax3prime != []:
+            #    print(np.char.find(refGenome[0], minMax3prime[0]) + len(minMax3prime[1]), readPairs[i][0], allMinMaxSubstrings[i])
         else:
             allMinMaxSubstrings[i].append(0)
             allMinMaxSubstrings[i].append([])
@@ -83,23 +88,79 @@ for i in range(totalReadPairs):
     elif np.char.find(refGenome[0], readPairs[i][1]) == -1 and np.char.find(refGenome[0], readPairs[i][0]) == -1:
         allMinMaxSubstrings[i] == None
 
-breakPoints = []
+breakPointsDeletions = []
 for i in range(totalReadPairs):
     if allMinMaxSubstrings[i] != []:
         if allMinMaxSubstrings[i][1] != [] and allMinMaxSubstrings[i][2] != []:
-            allPossibleReconstructions = []
-            pairIndex = allMinMaxSubstrings[i][0]
-            for x in range(len(allMinMaxSubstrings[i][1])):
-                for y in range(len(allMinMaxSubstrings[i][2])):
-                    reconstruction = allMinMaxSubstrings[i][1][x] + allMinMaxSubstrings[i][2][y]
-                    if reconstruction == readPairs[i][pairIndex]:
-                        breakPoint01 = np.char.find(refGenome[0], allMinMaxSubstrings[i][1][x]) + len(allMinMaxSubstrings[i][1][x])
-                        breakPoint02 = np.char.find(refGenome[0], allMinMaxSubstrings[i][2][y]) + 0
-                        breakPoints.append([i, pairIndex, breakPoint01, breakPoint02])
-                    allPossibleReconstructions.append(reconstruction)
-            if readPairs[i][pairIndex] in allPossibleReconstructions:
-                print(i, readPairs[i][pairIndex])
-                print(allMinMaxSubstrings[i])
-for item in breakPoints:
+            if len(allMinMaxSubstrings[i][1][1]) + len(allMinMaxSubstrings[i][2][1]) >= readLength:
+                pairIndex = allMinMaxSubstrings[i][0]
+                leftBreakPoint = np.char.find(refGenome[0], allMinMaxSubstrings[i][1][1]) + len(allMinMaxSubstrings[i][1][1])
+                rightBreakPoint = np.char.find(refGenome[0], allMinMaxSubstrings[i][2][1]) + 0
+                breakPointsDeletions.append([leftBreakPoint, rightBreakPoint])
+
+# Looking for insertion events in the reference genome
+allMinMaxSubstrings = []
+for i in range(totalReadPairs):
+    allMinMaxSubstrings.append([])
+    # If left read is mapped and right is not, start looking from 3' end of left read
+    if np.char.find(refGenome[0], readPairs[i][0]) > -1 and np.char.find(refGenome[0], readPairs[i][1]) == -1:
+        anchorPoint = np.char.find(refGenome[0], readPairs[i][0]) + readLength
+        minMax3prime = minmaxSubstrings(refGenome[0][anchorPoint:anchorPoint + 2*insertSize], readPairs[i][1], reverse=False)
+        if minMax3prime != []:
+            threePrimeAnchor = np.char.find(refGenome[0][anchorPoint:anchorPoint + 2*insertSize], minMax3prime[0])
+            minMax5prime = minmaxSubstrings(refGenome[0][anchorPoint + threePrimeAnchor:anchorPoint + threePrimeAnchor + readLength - 1],
+                                            readPairs[i][1], reverse=True)
+            allMinMaxSubstrings[i].append(1)
+            allMinMaxSubstrings[i].append(minMax3prime)
+            allMinMaxSubstrings[i].append(minMax5prime)
+            #print(np.char.find(refGenome[0], minMax3prime[0]) + len(minMax3prime[1]), readPairs[i][1], allMinMaxSubstrings[i])
+        else:
+            allMinMaxSubstrings[i].append(1)
+            allMinMaxSubstrings[i].append([])
+            allMinMaxSubstrings[i].append([])
+
+    # If right read is mapped and left is not, look upto the 3' end of right read
+    elif np.char.find(refGenome[0], readPairs[i][0]) == -1 and np.char.find(refGenome[0], readPairs[i][1]) > -1:
+        anchorPoint = np.char.find(refGenome[0], readPairs[i][1])
+        minMax5prime = minmaxSubstrings(refGenome[0][max(0, anchorPoint - 2*insertSize):anchorPoint], readPairs[i][0], reverse=True)
+        if minMax5prime != []:
+            fivePrimeAnchor = np.char.find(refGenome[0][max(0, anchorPoint - 2*insertSize):anchorPoint][::-1], minMax5prime[0][::-1])
+            minMax3prime = minmaxSubstrings(refGenome[0][max(0, anchorPoint - fivePrimeAnchor - readLength + 1)
+                                                         :anchorPoint - fivePrimeAnchor], readPairs[i][0], reverse=False)
+            allMinMaxSubstrings[i].append(0)
+            allMinMaxSubstrings[i].append(minMax3prime)
+            allMinMaxSubstrings[i].append(minMax5prime)
+            #if minMax3prime != []:
+            #    print(np.char.find(refGenome[0], minMax3prime[0]) + len(minMax3prime[1]), readPairs[i][0], allMinMaxSubstrings[i])
+        else:
+            allMinMaxSubstrings[i].append(0)
+            allMinMaxSubstrings[i].append([])
+            allMinMaxSubstrings[i].append([])
+    # If both reads are mapped or none is, there is nothing to be done
+    elif np.char.find(refGenome[0], readPairs[i][1]) > -1 and np.char.find(refGenome[0], readPairs[i][0]) > -1:
+        allMinMaxSubstrings[i] == None
+    elif np.char.find(refGenome[0], readPairs[i][1]) == -1 and np.char.find(refGenome[0], readPairs[i][0]) == -1:
+        allMinMaxSubstrings[i] == None
+
+breakPointsInsertions = []
+for i in range(totalReadPairs):
+    if allMinMaxSubstrings[i] != []:
+        if allMinMaxSubstrings[i][1] != [] and allMinMaxSubstrings[i][2] != []:
+            breakPoint01 = np.char.find(refGenome[0], allMinMaxSubstrings[i][1][1]) + len(allMinMaxSubstrings[i][1][1])
+            insertionLength = readLength - (len(allMinMaxSubstrings[i][1][1]) + len(allMinMaxSubstrings[i][2][1]))
+            breakPointsInsertions.append([breakPoint01, insertionLength])
+
+for item in breakPointsInsertions:
+    if breakPointsInsertions.count(item) < 2:
+        breakPointsInsertions.remove(item)
+
+print("Reporting deletions: ")
+breakPointsDeletions.sort(key=itemgetter(0))
+for item in breakPointsDeletions:
+    print(item)
+
+print("Reporting insertions: ")
+breakPointsInsertions.sort(key=itemgetter(0))
+for item in breakPointsInsertions:
     print(item)
 
--- PindelFunctions.py
+++ PindelFunctions.py
@@ -9,21 +9,6 @@ from decimal import *
 import math
 import numpy as np
 
-# Convert number to base 2 string
-def intoBase2String(number):
-    binaryConversion = "{0:b}".format(number)
-    return binaryConversion.zfill(12)
-
-# Return reverse compliment of the string
-def reverseCompliment(sequence):
-    sequence = sequence[::-1]
-    reverseSequence = ''
-    for nucleotide in sequence:
-        if nucleotide == "A": reverseSequence+="T"
-        elif nucleotide == "T": reverseSequence+="A"
-        elif nucleotide == "G": reverseSequence+="C"
-        elif nucleotide == "C": reverseSequence+="G"
-    return reverseSequence
 
 # Find minimum and maximum unique substrings
 def minmaxSubstrings(genome, pattern, reverse):
@@ -47,21 +32,3 @@ def minmaxSubstrings(genome, pattern, reverse):
         finalSubStrings.append(subStrings[0][::((-1*(reverse == True)) + (1*(reverse == False)))])
         finalSubStrings.append(subStrings[len(subStrings) - 1][::((-1*(reverse == True)) + (1*(reverse == False)))])
     return finalSubStrings
-
-# Detect a deletion event
-def detectDeletion(genome, read01, read02, insertionSize, maxDeletionSize):
-    # Locate read01 location on genome
-    read01Anchor = np.char.find(genome, read01) + len(read01)
-    # Find minimum maximum substrings from 3-prime end of second read
-    minmax3prime = minmaxSubstrings(genome[read01Anchor:(read01Anchor + 2*insertionSize)], read02, reverse=False)
-    if minmax3prime != []:
-        # Anchor the beginning of substring defined above in the genome
-        read02Anchor3prime = read01Anchor + np.char.find(genome[read01Anchor:(read01Anchor + 2*insertionSize)], minmax3prime[0])
-        # Find minimum maximum substrings from 5-prime end of second read
-        minmax5prime = minmaxSubstrings(genome[read02Anchor3prime:(read02Anchor3prime+maxDeletionSize+len(read02))], read02, reverse=True)
-        # Reverese the substrings from 5-prime end, since read02 runs from 3-prime to 5-prime
-        for i in range(len(minmax5prime)): minmax5prime[i] = minmax5prime[i][::-1]
-        return [minmax3prime, minmax5prime]
-    else:
-        return []
-
